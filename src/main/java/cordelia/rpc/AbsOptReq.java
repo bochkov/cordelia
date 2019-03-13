07f7d4b42b339e827acae28689b3e837f4b7cf29
Sergey Bochkov <bochkovsa@gmail.com>
2019-03-13 20:01:58 +0500

    fix not serializeble map

--- src/main/java/cordelia/rpc/AbsOptReq.java
+++ src/main/java/cordelia/rpc/AbsOptReq.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public abstract class AbsOptReq extends AbsReq implements OptReq {
 
-    protected final transient Map<String, Object> arguments;
+    protected final Map<String, Object> arguments;
 
     public AbsOptReq(Integer tag, String method, Map<String, Object> args) {
         super(tag, method);
