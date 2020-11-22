8a8f235d3b971e015dc8c54bf566391863073929
Sergey Bochkov <BochkovSA@cbr.ru>
2020-11-22 16:23:53 +0500

    fix test

--- src/test/java/cordelia/client/TrClientTest.java
+++ src/test/java/cordelia/client/TrClientTest.java
@@ -1,18 +1,8 @@
 package cordelia.client;
 
-import cordelia.rpc.FreeSpace;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 
 @Slf4j
 public class TrClientTest {
 
-    private static final String URL = "http://10.10.10.10:9091/transmission/rpc";
-
-    @Test
-    public void test0() {
-        Client client = new TrClient(URL);
-        TrResponse response = client.post(new FreeSpace("/storage/sdb/Downloads"), TrResponse.class);
-        LOG.info("{}", response.arguments());
-    }
 }
